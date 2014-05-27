2e32a387a89aa46ad6848803ce4cddcc23d0d94f
S.H.Kim <kimsh3933@gmail.com>
2014-05-27 12:04:40 +0900

    Initial Commit

---
+++ CardHeroes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardHeroes
+//
+//  Created by KimSH on 5/27/14.
+//  Copyright (c) 2014 COMP420. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CardHeroes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CardHeroes
+//
+//  Created by KimSH on 5/27/14.
+//  Copyright (c) 2014 COMP420. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
